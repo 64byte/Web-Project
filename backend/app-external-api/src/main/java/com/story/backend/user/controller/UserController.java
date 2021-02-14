8f678784f148bc5e337253b7d1f82b372c7475b6
64byte <8byte@naver.com>
2021-02-14 20:07:23 +0900

    Update UserController.java

--- backend/app-external-api/src/main/java/com/story/backend/user/controller/UserController.java
+++ backend/app-external-api/src/main/java/com/story/backend/user/controller/UserController.java
@@ -21,7 +21,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/users")
-public class asdasdaddadadadUserController {
+public class UserController {
 
     private final UserService userService;
 
